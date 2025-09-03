df8f0c8f8f2f04230cdfa9330bdc96741a612cb4
Veritas <ochigbogodswill868@gmail.com>
2025-09-03 16:11:18 -0700

    updating api endpoint

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://kovecaps_api.test/", 
+  baseURL: "https://e-commerce-api-1-nsvz.onrender.com", 
   withCredentials: true, 
     headers: {
     "Content-Type": "application/json",
