fcdbbe7c416b2ac31ba9f58cd52b03e521ae5d83
Veritas <ochigbogodswill868@gmail.com>
2025-09-08 14:17:11 -0700

    updated auth

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Banner from "../components/home/Banner"
 import Categories from "../components/home/Categories"
 import Footer from "../components/general/Footer"
@@ -7,8 +7,28 @@ import Navbar from "../components/general/Navbar"
 import News from "../components/home/News"
 import Products from "../components/home/Products"
 import Testimonials from "../components/home/Testimonials"
+import { useToast } from '../context/ToastContext'
+import { useLocation } from 'react-router-dom'
 
 const Home = () => {
+   const { showToast } = useToast();
+  
+    const location = useLocation();
+    const queryParams = new URLSearchParams(location.search);
+    const status = queryParams.get("status");
+    const reference = queryParams.get("reference");
+
+
+      useEffect(() => {
+    if (status === "success") {
+      showToast("Wallet funded successfully", "success");
+
+    } else if (status === "failed") {
+      alert(" Payment failed. Please try again.");
+      showToast("Wallet funding failed", "error");
+
+    }
+  }, [status, reference, showToast]);
   return (
     <>
         <Hero/>
